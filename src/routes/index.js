0d29b8ea446cfef33bb3ee9ccdcd3c73eb14471c
guebebraga <guebe.braga5@gmail.com>
2023-10-22 07:28:16 -0300

    descomente

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,10 @@
 const express = require('express')
 const router = express.Router();
 const controlersUser = require('../controlers/index')
-/*const controlersCarreras = require('../controlers/carreras')*/
 
-//const middlewares = require('../middlewares/logged')
 //router.use(middlewares.logged)
 
 router.post('/users', controlersUser.post)
-/*router.get('/users', middlewares.logged, controlersUser.get)*/
-/*router.post('/carreras', middlewares.logged, controlersCarreras.post)*/
 
 
 module.exports= router
--- src/routes/users.js
+++ src/routes/users.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router();
 const controlersUser = require('../controlers/index')
 const middlewares = require('../middlewares/logged')
+
 router.use(middlewares.logged)
 
 router.get('/users', controlersUser.get)
