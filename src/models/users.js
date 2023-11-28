2fb53ae850f3ef3648db0c1012592cbf6f381b58
guebebraga <guebe.braga5@gmail.com>
2023-11-28 00:41:34 -0300

    se agrego exists en get

--- src/models/users.js
+++ src/models/users.js
@@ -1,7 +1,6 @@
 const mongoose = require('../config/mongo')
-//const ObjectId = mongoose.Types.ObjectId
 const bcrypt = require('bcrypt');
-const redisClient = require ("../config/redis");//vichar sintaxis
+const redisClient = require ("../config/redis");
 require('dotenv').config();
 
 const userSchema = new mongoose.Schema({
@@ -18,21 +17,18 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model('users', userSchema);
 
-/*
-let objectCache = await redisClient.HGET(collectionName, id)
-  if(objectCache){
-    console.log("retorno desde cache")
-    return JSON.parse(objectCache)
-  }
-
-*/
 
 async function profile(_id) {
   try {
-    let profileCache = await redisClient.get(_id.toString()); //aca explota si le saco toString()
+   let profileCache = await redisClient.get(_id.toString()); 
+   /* redisClient.get(_id.toString(), (err, reply) => console.log(reply))*/
+   /* redisClient.exists(_id.toString())*/
+   let existe = await redisClient.exists(_id.toString())
+   console.log(existe)
+
     if (profileCache) {
       console.log('De redis');
-      // Parse the cached profile before returning it
+     
       return JSON.parse(profileCache);
     }
     const profile = await User.findOne({ _id: _id });
@@ -42,36 +38,7 @@ async function profile(_id) {
     throw `Imposible retornar ${error}`;
   }
 }
-/*
-async function get(user, pass){
-  try{
-    const res = await User.findOne({username:user})
-    console.log(res)
-    const compare = await bcrypt.compare(pass, res.password)
-    console.log(compare)
-    if(!compare){
-      throw ('contraseña incorrecta')
-    }
-    console.log(user,pass)
-    //guardo en cache 
-    const userCache = JSON.stringify({
-      _id:res._id,
-      nombre:res.nombre,
-      apellido:res.apellido,
-      rol:res.rol
-     })
-     console.log(userCache)
-     redisClient.set(res._id.valueOf(),userCache,{EX:parseInt(process.env.REDIS_TTL)})
-     console.log(res._id.valueOf())
-     console.log('guardado en cache')
-     return res
-     
-  }
-  catch (error) {
-      throw (`imposible retornar ${error}`)
-  }
-}
-*/
+
 async function get(user, pass) {
   try {
     const res = await User.findOne({ username: user });
@@ -89,13 +56,17 @@ async function get(user, pass) {
       apellido: res.apellido,
       rol: res.rol,
     });
-    // Guardo en cache
+    
+    console.log('Guardado en cache');
     redisClient.set(res._id.valueOf(), userCache, {
       EX: parseInt(process.env.REDIS_TTL),
     });
-    console.log('Guardado en cache');
+
     console.log(userCache)
-    // Solo retornar res si la comparación de contraseñas es exitosa
+    console.log('Compruebo si esta en cache')
+    let existe = await redisClient.exists(res._id.toString())
+    console.log(existe)
+    
     return res;
   } catch (error) {
     throw `Imposible retornar ${error}`;
